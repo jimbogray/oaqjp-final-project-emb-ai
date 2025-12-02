92c78b471145a7ccab99f94f18f22f870c528215
jimbogray <jimbogray@gmail.com>
2025-12-01 21:32:13 -0500

    done

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+"""Web app with endpoint for assessing emotional features of input string."""
 import ast
 
 from flask import Flask, request, make_response
@@ -8,33 +9,35 @@ app = Flask("__name__")
 @app.route('/emotionDetector')
 # ‘/’ URL is bound with hello_world() function.
 def emotion_detector():
+    """Handler for emotion detector, reads textToAnalyze param 
+    and passes to emotion detection function."""
     template = "For the given statement, the system response is "
-    template += "'anger': {0}, "
-    template += "'disgust': {1}, "
-    template += "'fear': {2}, "
-    template += "'joy': {3} and "
-    template += "'sadness': {4}. "
-    template += "The dominant emotion is {5}."
+    template += "'anger': {}, "
+    template += "'disgust': {}, "
+    template += "'fear': {}, "
+    template += "'joy': {} and "
+    template += "'sadness': {}. "
+    template += "The dominant emotion is {}."
 
-    textToAnalyze = request.args.get('textToAnalyze')
+    text_to_analyze = request.args.get('textToAnalyze')
 
-    if not textToAnalyze:
+    if not text_to_analyze:
         res = make_response(template.format(None, None, None, None, None, None))
         res.status_code = 400
         return res
 
-    emotions = emotion_detection.emotion_detector(textToAnalyze)
+    emotions = emotion_detection.emotion_detector(text_to_analyze)
     emotions = ast.literal_eval(emotions)
 
     if emotions['dominant_emotion'] is None:
         return make_response("Invalid text! Please try again!")
 
     res = make_response(
-        template.format(emotions['anger'], 
-            emotions['disgust'], 
-            emotions['fear'], 
-            emotions['joy'], 
-            emotions['sadness'], 
+        template.format(emotions['anger'],
+            emotions['disgust'],
+            emotions['fear'],
+            emotions['joy'],
+            emotions['sadness'],
             emotions['dominant_emotion'])
         )
 
